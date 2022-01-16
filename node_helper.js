736c58aa54219af9849fd4450b58600d08d9a4a6
Aldarande <72162771+Aldarande@users.noreply.github.com>
2022-01-16 20:58:30 +0100

    Get data from API AirParif 2.0
    Update data to get data from Airparif 2.0

--- node_helper.js
+++ node_helper.js
@@ -1,14 +1,15 @@
 var NodeHelper = require("node_helper");
 var request = require("request");
+var log;
 
 module.exports = NodeHelper.create({
 	start: function() {
 		console.log("Démarrage du node_helper de MMM-AirParif...");
-		this.API = "http://www.airparif.asso.fr/services/api/1.0/idxville?villes="
+		this.API = "https://api.airparif.asso.fr/indices/prevision/commune?insee="
 		this.url = ""
-		this.codeInsee = []
-		this.Village = []
-		this.data = []
+		this.codeInsee = [];
+		this.Village = [];
+		this.data = [];
 		this.SCAN = {
 				"ville": [],
 				"data" : []
@@ -18,30 +19,33 @@ module.exports = NodeHelper.create({
 	/*** Script de recuperation de toutes les infos ***/
 	getData: function(payload) {
 		var self = this
-
-		for(var i in self.config.ville) this.insee(self.config.ville[i],i) // Recupere les codes INSEE des villes
+		
+		
+		for(var i in self.config.ville) {
+		this.insee(self.config.ville[i],i) // Recupere les codes INSEE des villes
+		
 
 		setTimeout(() => {
 			this.url = self.API
 			for(var i in self.codeInsee) { // fabrication de l'URL avec les codes INSEE
-				this.url += self.codeInsee[i]
-				if(i != (self.codeInsee.length-1)) this.url += ","
-				else this.url += ("&key=" + this.config.key)
+				
+				if(i == 0) this.url += self.codeInsee[i]
+				else this.url += ("&insee=" + self.codeInsee[i])
 			}
+			
 			this.getAir(this.url); // envoi vers l'api de AirParif
 		} ,4000);
-
+	}
 		setTimeout(() => {
-			for(var i in self.data) {
-				for (var j in self.codeInsee) { // verifie si les codes INSEE sont bien en region parisienne
-					if(self.data[i].ninsee == self.codeInsee[j]) { // stock les données si ok !
-						self.SCAN.ville[i] = self.Village[j]
-						self.SCAN.data[i] = self.data[i].jour
-					}
+			for (var i in self.codeInsee) {
+				for (var j in self.data[self.codeInsee[i]]) { 
+						self.SCAN.ville[i] = self.Village[i]
+						self.SCAN.data[i] = self.data[self.codeInsee[i]];						
 				}
-			}
-			console.log("[AirParif] SCAN Effectué")
-			if (self.SCAN.ville[0] == null) self.sendSocketNotification("RESULT", false); // pas de villes retournées (erreur)
+			}						
+			console.info("[AirParif] SCAN Effectué")
+
+			if (self.SCAN.ville[i] == null) self.sendSocketNotification("RESULT", false); // pas de villes retournées (erreur)
 			else self.sendSocketNotification("RESULT", self.SCAN); // envoie les donnnées
 		} , 6000);
 
@@ -52,16 +56,16 @@ module.exports = NodeHelper.create({
 		var self = this
 		this.getAir(this.url);
 		setTimeout(() => {
+
                 	for(var i in self.data) {
-                   	   for (var j in self.codeInsee) {
-               	      	      if(self.data[i].ninsee == self.codeInsee[j]) {
-                      		self.SCAN.ville[i] = self.Village[j]
-                        	self.SCAN.data[i] = self.data[i].jour
-                      	      }
+                   	   for (var j in self.data[self.codeInsee[i]]) {
+								self.SCAN.ville[i] = self.Village[i]
+								self.SCAN.data[i] = self.data[i].jour
                    	   }
                 	}
-			console.log("[AirParif] SCAN Update Effectué")
-                	if (self.SCAN.ville[0] == null) self.sendSocketNotification("RESULT", false);
+					console.log("[AirParif] SCAN Update Effectué");
+
+					if (self.SCAN.ville[0] == null) self.sendSocketNotification("RESULT", false);
                 	else self.sendSocketNotification("RESULT", self.SCAN);
 		} , 4000);
 	},
@@ -70,8 +74,8 @@ module.exports = NodeHelper.create({
 	/*** requete vers l'API de AirParif ***/
 	getAir: function(url) {
 		var self = this;
-		request({ url: url, method: "GET" }, function(error, response, body) {
-			var result
+		request({ url: url, method: "GET", headers: {'User-Agent':'request','X-Api-Key': self.config.key } }, function(error, response, body) {
+			var result;
 			if (!error && response.statusCode == 200) { 
 				result = JSON.parse(body);
 				if (result.status == "error") {
@@ -84,7 +88,7 @@ module.exports = NodeHelper.create({
 					result = JSON.parse(body);
 					if (result.erreur) console.log("[AirParif][API] Erreur: " + result.erreur)
 				}
-				else console.log("[AirParif][API] Erreur: Aucune Donnée trouvé")
+				else console.info("[AirParif][API] Erreur: Aucune Donnée trouvé")
 			}
 		});
 	},
@@ -92,27 +96,29 @@ module.exports = NodeHelper.create({
   /*** Requete code INSEE vers geo.api.gouv.fr et formate la ville ***/
   insee: function(ville,i) {
     var self = this;
-    var str = ville
+    var str = ville;
     var split = str.split(',');
     var codepostal = split[0];
     self.Village[i] = split[1];
+	
     if(codepostal.substring(0,2) == "75") {
       let dep = codepostal.substring(0,2)
       let arr = codepostal.substring(3,5)
       self.codeInsee[i] = dep+"1"+arr
+
     } else {
       var url = 'https://geo.api.gouv.fr/communes?codePostal=' + codepostal + '&nom=' + self.Village[i] + '&fields=nom,code,codesPostaux&format=json';
-      request.get({ url: url, json: true, headers: {'User-Agent': 'request'} }, (err, res, data) => {
+	  request.get({ url: url, json: true, headers: {'User-Agent': 'request'} }, (err, res, data) => {
         if (err) {
-          console.log("[AirParif][INSEE] " + err);
+			console.log("[AirParif][INSEE] " + err);
         } else {
           if (res.statusCode !== 200) console.log("[AirParif][INSEE] Erreur Status Code:", res);
           else {
             if (!data[0]) {
-              console.log("[AirParif][INSEE] Erreur Aucune Données !")
+				console.log("[AirParif][INSEE] Erreur Aucune Données !")
             } else {
               self.codeInsee[i] = data[0].code;
-              self.Village[i] = data[0].nom;
+			  self.Village[i] = data[0].nom;
             }
           }
         }
