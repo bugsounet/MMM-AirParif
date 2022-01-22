7069b8279bc5b9e0f5e0c7abd33a081e287d7f24
bugsounet <github@bugsounet.fr>
2022-01-23 00:34:57 +0100

    V2.0.0

--- MMM-AirParif.js
+++ MMM-AirParif.js
@@ -1,185 +1,141 @@
 /* Magic Mirror Module: MMM-AirParif
  *  Version 2.0.0
- *  by Aldarande
- * compatible avec le version API Airparif 2.0.0 
+ *  by @bugsounet, Aldarande
+ * compatible avec la version API Airparif 2.0.0
  */
 
-var log;
+logAIR = (...args) => { /* do nothing */ }
 
 Module.register("MMM-AirParif", {
-	defaults: {
-		key: "",
-		ville: [],
-		polluants: false,
-		demain : false,
-		update: 60,
-		demain: true,
-	},
+  defaults: {
+    debug: false,
+    key: "",
+    ville: [],
+    polluants: false,
+    demain: false,
+    update: 60*60*1000,
+  },
 
-	start: function () {
-		Log.info("Starting module: " + this.name);
-		this.config = this.configAssignment({}, this.defaults, this.config);
-		this.loaded = false;
-		this.result = {};
-		
-	},
+  start: function () {
+    this.config = configMerge({}, this.defaults, this.config)
+    this.loaded = false
+    this.result = {}
+  },
 
-	getStyles: function () {
-		return ["MMM-AirParif.css", "weather-icons.css"];
-	},
+  getStyles: function () {
+    return ["MMM-AirParif.css", "weather-icons.css"]
+  },
 
-	getScripts: function () {
-		return ["moment.js"];
-	},
+  getScripts: function () {
+    return ["moment.js"]
+  },
 
-	getHeader: function () {
-		return this.data.header;
-	},
+  getHeader: function () {
+    return this.data.header
+  },
 
-	getDom: function () {
-		var self = this;
-		var result = this.result;
-		var wrapper = document.createElement("div");
-		
-		if (self.config.key == "") {
-			wrapper.innerHTML = "Erreur: Merci de définir la Clé (key).";
-			wrapper.className = "dimmed light small";
-			return wrapper;
-		}
+  getDom: function () {
+    var result = this.result
+    var wrapper = document.createElement("div")
 
-		if (!this.loaded) {
-			wrapper.innerHTML = this.translate("LOADING");
-			wrapper.className = "dimmed light small";
-			return wrapper;
-		}
-		
-		// Start building table.
-		var dataTable = document.createElement("table");
-		dataTable.className = "small data";
-		
-		if(Object.keys(result).length > 0) {		   
-				for (var i in result.ville) {			
-				var Row = document.createElement("tr");
-				var ville = result.ville[i];
-				var ind = result.data[i];
-				
-				var indClass = "";
-				var villeCell = document.createElement("th");
-				villeCell.className = "ville " + ville;
-				villeCell.innerHTML = ville + " : ";
-				Row.appendChild(villeCell);
-				
-				var indCellTD = document.createElement("td");
-				indCellTD.className = "indiceP " + ind[0].indice.replace(/ /g, "");
-				indCellTD.innerHTML = ind[0].indice;
-				Row.appendChild(indCellTD);	
-				
+    if (this.config.key == "") {
+      wrapper.innerHTML = "Erreur: Merci de définir la Clé (key)."
+      wrapper.className = "dimmed light small"
+      return wrapper
+    }
 
-				if (this.config.demain == true) {
+    if (!this.loaded) {
+      wrapper.innerHTML = this.translate("LOADING");
+      wrapper.className = "dimmed light small";
+      return wrapper
+    }
 
-				var indCellTM = document.createElement("td");
-				indCellTM.className = "indiceP " + ind[1].indice.replace(/ /g, "");
-				indCellTM.innerHTML = ind[1].indice;
-				Row.appendChild(indCellTM);	
-				}
+    // Start building table.
+    var dataTable = document.createElement("table")
+    dataTable.className = "small data"
+    if (result && Object.entries(result).length) { // @todo scan array data present
+      Object.entries(result).forEach(([code, data]) => {
+        var Row = document.createElement("tr")
+        var ville = data.ville
+        var ind = data.data
 
-				dataTable.appendChild(Row);
-				
-				if (this.config.polluants){
+        var indClass = ""
+        var villeCell = document.createElement("th")
+        villeCell.className = "ville " + ville
+        villeCell.innerHTML = ville + ": "
+        Row.appendChild(villeCell)
 
-						for (var j = 1 ; j < Object.keys(ind[0]).length-1 ; j++) {
+        var indCellTD = document.createElement("td")
+        indCellTD.className = "indiceP " + ind[0].indice.replace(/ /g, "")
+        indCellTD.innerHTML = ind[0].indice
+        Row.appendChild(indCellTD)
 
-							var PolLig = document.createElement("tr");
-							PolLig.className = "Polluants" ; //Object.keys(ind[0])[j];
+        if (this.config.demain == true && (ind[1] && ind[1].indice)) {
+          var indCellTM = document.createElement("td")
+          indCellTM.className = "indiceP " + ind[1].indice.replace(/ /g, "")
+          indCellTM.innerHTML = ind[1].indice
+          Row.appendChild(indCellTM)
+        }
 
-							var PolCellLig = document.createElement("th");
-							PolCellLig.style.listStyleType = "none";
-							PolCellLig.className = "ligne";
-							PolCellLig.innerHTML = Object.keys(ind[0])[j];
-							PolLig.appendChild(PolCellLig);
-							
-							var PolCellTD = document.createElement("td");
-							PolCellTD.style.listStyleType = "none";
-							PolCellTD.className = "indice " + ind[0][Object.keys(ind[0])[j]].replace(/ /g, "");
-							PolCellTD.innerHTML =  ind[0][Object.keys(ind[0])[j]];
-							PolLig.appendChild(PolCellTD);
+        dataTable.appendChild(Row)
 
-							if (this.config.demain == true) {
-							var PolCellTM = document.createElement("td");
-							PolCellTM.style.listStyleType = "none";
-							PolCellTM.className = "indice " + ind[1][Object.keys(ind[0])[j]].replace(/ /g, "");
-							PolCellTM.innerHTML =  ind[1][Object.keys(ind[0])[j]];
-							PolLig.appendChild(PolCellTM);
-							}
-							dataTable.appendChild(PolLig);							
-					}			
-				}
-			}
-		} else {
-			var row1 = document.createElement("tr");
-			dataTable.appendChild(row1);
+        if (this.config.polluants) { //@todo better
+          for (var j = 1 ; j < Object.keys(ind[0]).length-1 ; j++) {
+            var PolLig = document.createElement("tr")
+            PolLig.className = "Polluants"
 
-			var messageCell = document.createElement("td");
-			messageCell.innerHTML = "Erreur: Aucune Donnée";
-			messageCell.className = "bright";
-			row1.appendChild(messageCell);
-		}
-	
-		wrapper.appendChild(dataTable);
-		return wrapper;
-	},
+            var PolCellLig = document.createElement("th")
+            PolCellLig.style.listStyleType = "none"
+            PolCellLig.className = "ligne"
+            PolCellLig.innerHTML = Object.keys(ind[0])[j]
+            PolLig.appendChild(PolCellLig)
 
-	notificationReceived: function (notification, payload) {
-        	if (notification === 'DOM_OBJECTS_CREATED') {
-            		//DOM creation complete, let's start the module
-            		this.sendSocketNotification("SCAN", this.config);
-        	}
-	},
+            var PolCellTD = document.createElement("td")
+            PolCellTD.style.listStyleType = "none"
+            PolCellTD.className = "indice " + ind[0][Object.keys(ind[0])[j]].replace(/ /g, "")
+            PolCellTD.innerHTML =  ind[0][Object.keys(ind[0])[j]]
+            PolLig.appendChild(PolCellTD)
 
-	socketNotificationReceived: function (notification, payload) {
-		var self = this
-		if (notification === "RESULT" ) {
-			this.result = payload;
-			this.loaded = true;
-			this.UpdateInterval();
-		}
-	},
+            if (this.config.demain == true && (ind[1] && ind[1].indice)) {
+              var PolCellTM = document.createElement("td")
+              PolCellTM.style.listStyleType = "none"
+              PolCellTM.className = "indice " + ind[1][Object.keys(ind[0])[j]].replace(/ /g, "")
+              PolCellTM.innerHTML =  ind[1][Object.keys(ind[0])[j]]
+              PolLig.appendChild(PolCellTM)
+            }
+            dataTable.appendChild(PolLig)
+          }
+        }
+      })
+    } else {
+      var row1 = document.createElement("tr")
+      dataTable.appendChild(row1)
 
-  	configAssignment : function (config) {
-    		var stack = Array.prototype.slice.call(arguments, 1)
-    		var item
-   		var key
-    		while (stack.length) {
-      			item = stack.shift()
-      			for (key in item) {
-        			if (item.hasOwnProperty(key)) {
-          				if (typeof config[key] === "object" && config[key] && Object.prototype.toString.call(config[key]) !== "[object Array]") {
-            					if (typeof item[key] === "object" && item[key] !== null) {
-              						config[key] = this.configAssignment({}, config[key], item[key])
-            					} else {
-              						config[key] = item[key]
-            					}
-          				} else {
-            					config[key] = item[key]
-          				}
-        			}
-      			}
-    		}
-    		return config
-  	},
+      var messageCell = document.createElement("td")
+      messageCell.innerHTML = "Erreur: Aucune Donnée"
+      messageCell.className = "bright"
+      row1.appendChild(messageCell)
+    }
 
-        UpdateInterval: function () {
-        	var self = this;
-			clearInterval(self.interval);
-			self.counter = this.config.update * 60 * 1000;
-			self.updateDom(1000);
+    wrapper.appendChild(dataTable)
+    return wrapper
+  },
 
-        	self.interval = setInterval(function () {
-            		self.counter -= 1000;
-            		if (self.counter <= 0) {
-				clearInterval(self.interval);
-				self.sendSocketNotification("UPDATE");
-            		}
-        	}, 1000);
-        },	
+  notificationReceived: function (notification, payload) {
+    if (notification === 'DOM_OBJECTS_CREATED') {
+      //DOM creation complete, let's start the module
+      if (this.config.debug) logAIR = (...args) => { console.log("[AIRPARIF]", ...args) }
+      this.sendSocketNotification("INIT", this.config)
+      this.sendSocketNotification("SCAN")
+    }
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "RESULT" ) {
+      this.result = payload
+      this.loaded = true
+      logAIR(this.result)
+      this.updateDom()
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,139 +1,183 @@
 var NodeHelper = require("node_helper");
-var request = require("request");
-var log;
+var request = require("request-promise");
+
+logAIR = (...args) => { /* do nothing */ }
 
 module.exports = NodeHelper.create({
-	start: function() {
-		console.log("Démarrage du node_helper de MMM-AirParif...");
-		this.API = "https://api.airparif.asso.fr/indices/prevision/commune?insee="
-		this.url = ""
-		this.codeInsee = [];
-		this.Village = [];
-		this.data = [];
-		this.SCAN = {
-				"ville": [],
-				"data" : []
-		}
-	},
+  start: function() {
+    this.API = "https://api.airparif.asso.fr/indices/prevision/commune?insee="
+    this.url = ""
+    this.data= {}
+  },
 
-	/*** Script de recuperation de toutes les infos ***/
-	getData: function(payload) {
-		var self = this
-		
-		
-		for(var i in self.config.ville) {
-		this.insee(self.config.ville[i],i) // Recupere les codes INSEE des villes
-		
+  /*** Script de recuperation de toutes les infos ***/
+  getData: async function(payload) {
+    await this.insee(this.config.ville)
 
-		setTimeout(() => {
-			this.url = self.API
-			for(var i in self.codeInsee) { // fabrication de l'URL avec les codes INSEE
-				
-				if(i == 0) this.url += self.codeInsee[i]
-				else this.url += ("&insee=" + self.codeInsee[i])
-			}
-			
-			this.getAir(this.url); // envoi vers l'api de AirParif
-		} ,4000);
-	}
-		setTimeout(() => {
-			for (var i in self.codeInsee) {
-				for (var j in self.data[self.codeInsee[i]]) { 
-						self.SCAN.ville[i] = self.Village[i]
-						self.SCAN.data[i] = self.data[self.codeInsee[i]];						
-				}
-			}						
-			console.info("[AirParif] SCAN Effectué")
+    this.url = this.API
+    let nb=0
+    logAIR("getData:", this.data)
+    Object.entries(this.data).forEach(
+      ([key, value]) => {
+        if (nb == 0) this.url += key
+        else this.url += ("&insee=" + key)
+        nb++
+      }
+    )
+    logAIR("getData URL:", this.url)
+    await this.getAir(this.url) // envoi vers l'api de AirParif
 
-			if (self.SCAN.ville[i] == null) self.sendSocketNotification("RESULT", false); // pas de villes retournées (erreur)
-			else self.sendSocketNotification("RESULT", self.SCAN); // envoie les donnnées
-		} , 6000);
+    logAIR("getData SCAN Effectué")
+    if (Object.keys(this.data).length) this.sendSocketNotification("RESULT", this.data) // envoie les donnnées
+    else this.sendSocketNotification("RESULT", false) // pas de villes retournées (erreur)
+    setInterval(() => { this.getUpdate() }, this.config.update*1000)
+  },
 
-	},
+  /*** Update Script **/
+  /** new uniquement avec le node_helper **/
+  getUpdate: async function() {
+    await this.getAir(this.url)
+    logAIR("SCAN Update Effectué")
 
-	/*** Update Script **/
-	getUpdate: function() {
-		var self = this
-		this.getAir(this.url);
-		setTimeout(() => {
+    if (Object.keys(this.data).length) this.sendSocketNotification("RESULT", this.data) // envoie les donnnées
+    else this.sendSocketNotification("RESULT", false) // pas de villes retournées (erreur)
+  },
 
-                	for(var i in self.data) {
-                   	   for (var j in self.data[self.codeInsee[i]]) {
-								self.SCAN.ville[i] = self.Village[i]
-								self.SCAN.data[i] = self.data[i].jour
-                   	   }
-                	}
-					console.log("[AirParif] SCAN Update Effectué");
+  /*** requete vers l'API de AirParif ***/
+  getAir: function(url) {
+    logAIR("AirParif request:", url)
+    var ObjectTmp = {}
+    return new Promise(resolve => {
+      request({
+        url: url,
+        method: "GET",
+        headers: {'User-Agent':'Request-Promise','X-Api-Key': this.config.key },
+        json: true
+      })
+        .then(result => {
+          logAIR("Données reçu par AirParif:",result)
+          Object.entries(result).forEach(
+            ([key, value]) => {
+              logAIR("Placement des données pour",key)
+              ObjectTmp[key]= {
+                data: value
+              }
+            }
+          )
+          logAIR("Résulat:", ObjectTmp)
+          logAIR("Mélange des données...")
 
-					if (self.SCAN.ville[0] == null) self.sendSocketNotification("RESULT", false);
-                	else self.sendSocketNotification("RESULT", self.SCAN);
-		} , 4000);
-	},
-
-
-	/*** requete vers l'API de AirParif ***/
-	getAir: function(url) {
-		var self = this;
-		request({ url: url, method: "GET", headers: {'User-Agent':'request','X-Api-Key': self.config.key } }, function(error, response, body) {
-			var result;
-			if (!error && response.statusCode == 200) { 
-				result = JSON.parse(body);
-				if (result.status == "error") {
-					console.log("[AirParif][API] Erreur > "+ response.statusCode + " -- " + result)
-				} else {
-					self.data = result;
-				}
-			} else {
-				if (body) { 
-					result = JSON.parse(body);
-					if (result.erreur) console.log("[AirParif][API] Erreur: " + result.erreur)
-				}
-				else console.info("[AirParif][API] Erreur: Aucune Donnée trouvé")
-			}
-		});
-	},
+          this.data = this.Merged({}, this.data, ObjectTmp)
+          logAIR("Résultat final:", this.data)
+          resolve()
+        })
+        .catch (err => {
+          console.error("[AirParif][API] Erreur: " + err)
+          console.error("[AirParif][API] Erreur: Aucune Donnée trouvé pour url", url)
+          resolve()
+        })
+    })
+  },
 
   /*** Requete code INSEE vers geo.api.gouv.fr et formate la ville ***/
-  insee: function(ville,i) {
-    var self = this;
-    var str = ville;
-    var split = str.split(',');
-    var codepostal = split[0];
-    self.Village[i] = split[1];
-	
-    if(codepostal.substring(0,2) == "75") {
-      let dep = codepostal.substring(0,2)
-      let arr = codepostal.substring(3,5)
-      self.codeInsee[i] = dep+"1"+arr
-
-    } else {
-      var url = 'https://geo.api.gouv.fr/communes?codePostal=' + codepostal + '&nom=' + self.Village[i] + '&fields=nom,code,codesPostaux&format=json';
-	  request.get({ url: url, json: true, headers: {'User-Agent': 'request'} }, (err, res, data) => {
-        if (err) {
-			console.log("[AirParif][INSEE] " + err);
+  /** new: scan la config complete **/
+  insee: function(villes, index = 1) {
+    return new Promise(resolve => {
+      villes.forEach(async data => {
+        var split = data.split(',')
+        var codepostal = split[0]
+        var ville = split[1]
+        logAIR("insee send requestInsee:", codepostal, ville)
+        await this.requestInsee(codepostal, ville)
+        if (index >= villes.length) {
+          logAIR("insee: OK, les informations INSEE sont résolu!", "Nb villes:", villes.length, "/", index )
+          resolve()
         } else {
-          if (res.statusCode !== 200) console.log("[AirParif][INSEE] Erreur Status Code:", res);
-          else {
-            if (!data[0]) {
-				console.log("[AirParif][INSEE] Erreur Aucune Données !")
-            } else {
-              self.codeInsee[i] = data[0].code;
-			  self.Village[i] = data[0].nom;
+          logAIR("insee: Recheche du suivant...", index)
+          index++
+        }
+      })
+    })
+  },
+
+  requestInsee: function(codepostal, ville) {
+    return new Promise(resolve => {
+      let tmpData = new Object()
+      if (codepostal.substring(0,2) == "75") {
+        let arr = codepostal.substring(3,5)
+        let code = "751"+arr
+        tmpData = {
+          ville: ville,
+          codepostal: codepostal,
+          data: "Les données de AirParif seront ici ;)"
+        }
+        this.data[code]=tmpData
+        logAIR("requestInsee -75- " + ville + ":", this.data[code])
+        resolve()
+      } else {
+        var url = 'https://geo.api.gouv.fr/communes?codePostal=' + codepostal + '&nom=' + ville + '&fields=nom,code,codesPostaux&format=json'
+        request({
+          url: url,
+          json: true,
+          headers: {'User-Agent': 'Request-Promise'}
+        })
+          .then(data => {
+            let code = data[0].code
+            let nom = data[0].nom
+            tmpData = {
+              ville: nom,
+              codepostal: codepostal,
+              data: "Les données de AirParif seront ici ;)"
             }
+            this.data[code]=tmpData
+            logAIR("requestInsee " + ville + ":", this.data[code])
+            resolve()
+          })
+          .catch (err => {
+            if (err) console.error("[AirParif][INSEE] " + err)
+            console.error("[AirParif][INSEE] Erreur Aucune Données:", codepostal, ville)
+            resolve()
+          })
+      }
+    })
+  },
+
+  /*** Reception notification par Socket ***/
+  socketNotificationReceived: function(notification, payload) {
+    switch (notification) {
+      case "SCAN":
+        this.getData(this.config)
+        break
+      case "INIT":
+        this.config = payload
+        if (this.config.debug) logAIR = (...args) => { console.log("[AIRPARIF]", ...args) }
+        console.log("[AIRPARIF] " + require('./package.json').name + " Version:", require('./package.json').version)
+        break
+    }
+  },
+
+/** Tools Merge 2 objets in deep **/
+/** it's the configDeepMerge MM function **/
+  Merged: function (result) {
+    var stack = Array.prototype.slice.call(arguments, 1)
+    var item
+    var key
+    while (stack.length) {
+      item = stack.shift()
+      for (key in item) {
+        if (item.hasOwnProperty(key)) {
+          if (typeof result[key] === "object" && result[key] && Object.prototype.toString.call(result[key]) !== "[object Array]") {
+            if (typeof item[key] === "object" && item[key] !== null) {
+              result[key] = this.Merged({}, result[key], item[key])
+            } else {
+              result[key] = item[key]
+            }
+          } else {
+            result[key] = item[key]
           }
         }
-      });
+      }
     }
-	},
-
-	/*** Reception notification par Socket ***/
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "SCAN") {
-			this.config = payload;
-			this.getData(this.config);
-		}
-		if (notification === "UPDATE") {
-			this.getUpdate();
-		}
-	}
+    return result
+  }
 });
